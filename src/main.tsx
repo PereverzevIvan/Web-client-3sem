9ffe04cdcaaa20c93ed381a1f2da0ff5e17a0789
PereverzevIvan <peregh320@gmail.com>
2024-06-01 14:13:42 +0300

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppThemeProvider } from "./context/AppThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Web-client-3sem">
+    <HashRouter basename="/Web-client-3sem">
       <AppThemeProvider>
         <App />
       </AppThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
