eca875c5b7f0a62aa78ca8a0f7adcf5219e7fb2f
PereverzevIvan <peregh320@gmail.com>
2024-06-01 13:51:30 +0300

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Web-client-3sem/",
   plugins: [react()],
   test: {
     globals: true,
