17b567539f09a03da275b44ff24f45b0bf7eece1
PereverzevIvan <peregh320@gmail.com>
2024-06-01 14:16:15 +0300

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppThemeProvider } from "./context/AppThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/Web-client-3sem">
+    <BrowserRouter basename="/Web-client-3sem">
       <AppThemeProvider>
         <App />
       </AppThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Web-client-3sem/",
+  base: "Web-client-3sem",
   plugins: [react()],
   test: {
     globals: true,
